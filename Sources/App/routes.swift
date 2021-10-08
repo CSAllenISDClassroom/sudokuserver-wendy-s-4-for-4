ca3218911ea71ce17dfbac0cb64ddbbde4b66a37
Kian Durrett <kian@durrett.org>
2021-10-08 10:38:00 -0500

    adding files from rohans repository

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,109 +1,65 @@
+// useful video https://www.youtube.com/watch?v=Bml5-9RR4Hk
 import Vapor
-//var x = [:]
-var sudokuBoardsMap = [Int:[[Int]]]()
-var countBoards = 0
-func scrambler (rotat:[Int],track:Int) ->[Int]{
-    var count = 1
-    var fir = [Int]()
-    var sec = [Int]()
-    var f = [Int]()
-    for i in rotat{
-        if count <= track {
-            fir.append(i)
-            count += 1
-        }
-        else{
-            sec.append(i) //add to a seconf array
-            count += 1
-        }
-    }
-    f = sec + fir //combines the arrays together
-    return f
-}//call the function for every row could have been done more effiecently but couldnt think
+var gametraker = [[[Int?]]]()
+var gamenum = -1
 
-
-//also couldnt think of a better method to print them all in less lines
-func generateBoard()->[[Int]]{
-    var x = [1,2,3,4,5,6,7,8,9]
-    x.shuffle()
-    //shuffles to create a first line this is easier than using logic to try to generate a new line
-    print(x)
-    //prints the first line
-    //var counter = 1
-    ///stores all the lines for the sudoku board
-    var row2 = [Int]()
-    var row3 = [Int]()
-    var row4 = [Int]()
-    var row5 = [Int]()
-    var row6 = [Int]()
-    var row7 = [Int]()
-    var row8 = [Int]()
-    var row9 = [Int]()
-    row2 = (scrambler(rotat:x,track:3))
-    row3 = (scrambler(rotat:row2,track:3))
-    row4 = (scrambler(rotat:row3,track:1))
-    row5 = (scrambler(rotat:row4,track:3))
-    row6 = (scrambler(rotat:row5,track:3))
-    row7 = (scrambler(rotat:row6,track:1))
-    row8 = (scrambler(rotat:row7,track:3))
-    row9 = (scrambler(rotat:row8,track:3))
-    var board = Array(repeating: Array(repeating:0, count: 9), count: 9)
-
-
-    for c in 0..<9{
-
-        board[0][c] = x[c]
-        board[1][c] = row2[c]
-        board[2][c] = row3[c]
-        board[3][c] = row4[c]
-        board[4][c] = row5[c]
-        board[5][c] = row6[c]
-        board[6][c] = row7[c]
-        board[7][c] = row8[c]
-        board[8][c] = row9[c]
-
-    }
-
-    //board[1] = x
-    print(row2)
-    print(row3)
-    print(row4)
-    print(row5)
-    print(row6)
-    print(row7)
-    print(row8)
-    print(row9)
-    return board
+func gamereturn() -> [[Int?]]{
+    var x = [[Int?]]()
+    x = gametraker[0]
+    return x
 }
 func routes(_ app: Application) throws {
-    app.get("Veom"){ req -> String in
+    app.get { req in
         return "It works!"
     }
 
-    app.get("games",":id") { req -> [[Int]] in
-        let empty = Array(repeating: Array(repeating:0, count: 0), count: 0)
-
-
-
-
-        let countBoardIdstr = req.parameters.get("id")!
-        let countBoardId = Int(countBoardIdstr)
-        if let sudokuBoard = sudokuBoardsMap[countBoardId!] {
-
-            return sudokuBoard
-        }
-
-        return empty
+    app.get("hello") { req -> String in
+        return "hello"
     }
-    app.post("games") { req -> Int in
-
-        print("inside post")
-        countBoards+=1
-        print(countBoards)
-        var newSudokuBoard = generateBoard()
-        print(newSudokuBoard)
-        sudokuBoardsMap[countBoards] = newSudokuBoard
-
-        return countBoards
+    app.get("nohello") { req -> String in
+        return "498837"
     }
+    app.post("game","create",":diffuculty") { req -> String in
+        let diff = req.parameters.get("diffuculty", as: String.self)!
+        let tomp = Emptyier().callall(dof:diff)
+        gametraker.append(tomp)
+        gamenum += 1
+        return("\(gamenum)")
+
+    }
+    app.get("game","call",":gamenumb") { req -> String in
+        let gameumb = req.parameters.get("gamenumb", as: Int.self)!
+        var x = [[Int?]]()
+        x = gametraker[gameumb]
+
+        return ("\(x)")
+    }
+    app.get("game","cellcall",":gamenumb",":row",":cell") { req -> String in
+        let gameumb = req.parameters.get("gamenumb", as: Int.self)!
+        let rownumb = req.parameters.get("row", as: Int.self)!
+        let cellnumb = req.parameters.get("cell", as: Int.self)!
+        var x = [[Int?]]()
+        var y = [Int?]()
+        x = gametraker[gameumb]
+        y = x[rownumb]
+        let z = y[cellnumb]
+        return ("\(z)")
+    }
+    app.put("game","ccell",":gamenhub",":row",":cell",":changed") { req -> String in
+        let gamehub = req.parameters.get("gamenhub", as: Int.self)!
+        let rownumb = req.parameters.get("row", as: Int.self)!
+        let cellnumb = req.parameters.get("cell", as: Int.self)!
+        let changenumb = req.parameters.get("changed", as: Int.self)!
+
+        var x = [[Int?]]()
+        x = gametraker[gamehub]
+        var y = x[rownumb]
+        y[cellnumb] = changenumb
+        x[rownumb] = y
+        gametraker[gamehub] = x
+
+
+        return("hi")
+    }
+
 }

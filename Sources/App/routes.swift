d1776406dad31cff9d25adfd39658e3fab51892b
254176 <54588773+254176@users.noreply.github.com>
2021-10-20 09:54:25 -0500

    Delete unnecessary code

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -34,10 +34,10 @@ func routes(_ app: Application) throws {
         return("\(gameNumber)")
         
     }
-<<<<<<< HEAD
+
     app.get("game","call",":gameNumber") { req -> String in
         let gameNumber = req.parameters.get("gameNumber", as: Int.self)!
-=======
+
 
     //gets the sudoku board
     app.get("game","call",":gamenumb") { req -> String in
@@ -45,24 +45,20 @@ func routes(_ app: Application) throws {
         if gameumb >= gametracker.count{
             return("400 Bad Request (filter specified doesn't match requirements)")
         }
->>>>>>> 1525620de47081a95ca4833413e191a82a1d6b7e
+
         var x = [[Int?]]()
         x = gameTracker[gameNumber]
         
         return ("\(x)")
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
-=======
+
     app.get("game","cellCall",":gameNumber",":row",":cell") { req -> String in
         let gameNumber = req.parameters.get("gameNumber", as: Int.self)!
         let rowNumber = req.parameters.get("row", as: Int.self)!
         let cellNumber = req.parameters.get("cell", as: Int.self)!
->>>>>>> f295b1ba032f8108aeb79e1d63cfa307106d6c53
-=======
 
->>>>>>> 1525620de47081a95ca4833413e191a82a1d6b7e
+
+
     //gets the sudoku board
     app.get("game","cellcall",":gamenumb",":row",":cell") { req -> String in
         guard let gameumb = req.parameters.get("gamenumb", as: Int.self)
@@ -87,7 +83,7 @@ func routes(_ app: Application) throws {
             return("400 Bad Request (cellIndex is out of range 0 ... 8)")
         }
         
->>>>>>> 1525620de47081a95ca4833413e191a82a1d6b7e
+
         var x = [[Int?]]()
         var y = [Int?]()
         x = gameTracker[gameNumber]
@@ -95,14 +91,13 @@ func routes(_ app: Application) throws {
         let z = y[cellNumber]
         return ("\(z)")
     }
-<<<<<<< HEAD
+
     app.put("game","cell",":gameHub",":row",":cell",":changed") { req -> String in
         let gameHub = req.parameters.get("gameHub", as: Int.self)!
         let rowNumber = req.parameters.get("row", as: Int.self)!
         let cellNumber = req.parameters.get("cell", as: Int.self)!
         let changeNumber = req.parameters.get("changed", as: Int.self)!
 
-=======
     
     app.put("game","ccell",":gamenhub",":row",":cell",":changed") { req -> String in
         guard let gamehub = req.parameters.get("gamenhub", as: Int.self)
@@ -135,7 +130,7 @@ func routes(_ app: Application) throws {
             return("400 Bad Request (value is out of range 1 ... 9 or null)")
         }
         
->>>>>>> 1525620de47081a95ca4833413e191a82a1d6b7e
+
         var x = [[Int?]]()
          x = gameTracker[gameHub]
         var y = x[rowNumber]
